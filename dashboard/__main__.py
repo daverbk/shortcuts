dc16f44e05a74d07b1bbf2cb465c3f4b1d361113
David Rabko <david.rabko@icloud.com>
2025-02-22 12:03:16 +0300

    Add tests for main()

--- dashboard/__main__.py
+++ dashboard/__main__.py
@@ -1,20 +1,4 @@
-from update.update import Budget, Meeting, ToDo, Habit, Weather, Birthday, Headline
-
-
-def main():
-    updates = [
-        Budget(),
-        Meeting(),
-        ToDo(),
-        Habit(),
-        Weather(),
-        Birthday(),
-        Headline()
-    ]
-
-    for update in updates:
-        update.run()
-
+from update.update import main
 
 if __name__ == "__main__":
     main()
